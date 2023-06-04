c6b23a6c3b2a157c294d0b1ab39482b0a48ca69c
Sanjitha Amarathunga <sanji77th@gmail.com>
2023-06-04 16:49:03 +0530

    Data ingestion has completed

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,47 @@
+import sys
+import os
+from src.exception import customException
+from src.logger import logging
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str = os.path.join('artifacts',"train.csv")
+    test_data_path:str = os.path.join('artifacts',"test.csv")
+    raw_data_path:str = os.path.join('artifacts',"raw.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.data_ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered to the data ingestion method")
+        try:
+            df = pd.read_csv('notebook/data/student_data.csv')
+            logging.info("Read the dataset")
+
+            os.makedirs(os.path.dirname(self.data_ingestion_config.train_data_path),exist_ok=True)
+            df.to_csv(self.data_ingestion_config.raw_data_path,header=True,index=False)
+
+            logging.info("train test split is initiated")
+            train_set, test_set = train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.data_ingestion_config.train_data_path,header=True,index=False)
+            test_set.to_csv(self.data_ingestion_config.test_data_path,header=True,index=False)
+
+            logging.info("data ingestion is completed")
+
+            return(
+                self.data_ingestion_config.train_data_path,
+                self.data_ingestion_config.test_data_path
+            )
+
+        except Exception as e:
+            raise customException(e,sys)
+        
+if __name__=="__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
--- src/exception.py
+++ src/exception.py
@@ -5,7 +5,7 @@ def error_message_deatil(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     line_number = exc_tb.tb_lineno
-    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{3}]".format(
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name, line_number, str(error)   
     )
 
